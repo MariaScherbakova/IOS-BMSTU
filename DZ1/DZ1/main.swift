60eb28ce7b73f54badf6b6928f22a1b85442a2be
Maria Scherbakova <mrssue@yandex.ru>
2018-12-23 23:20:39 +0300

    create currency enum and implement Account

--- DZ1/DZ1/main.swift
+++ DZ1/DZ1/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Currency :String {
+    case RUR,USD,EUR
+}
+
+class Account {
+    var id: Int
+    var balance: Double
+    var currency: Currency
+    var Description: String
+    var Offer: String?
+    
+    init(_ id: Int, _ balance: Double, _ currency: String, _ Description: String, _ Offer: String? = nil){
+        self.id = id
+        self.balance = balance
+        self.currency = Currency(rawValue: currency)!
+        self.Description = Description
+        self.Offer = Offer
+    }
+    func print() -> String {
+        return """
+        User id: \(id)
+        Balance: \(balance) \(currency.rawValue)
+        Description: \(Description)
+        Offer: \(Offer ?? "None")
+        """
+    }
+}
+
+var acc = Account(1,2.5,"RUR","My account")
+print(acc.print())
 
