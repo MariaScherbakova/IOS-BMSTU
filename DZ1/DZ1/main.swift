7c1b49a72c55fe7dd9f664d7a0111c702193be7f
Maria Scherbakova <mrssue@yandex.ru>
2018-12-23 20:38:26 +0300

    Initial commit

---
+++ DZ1/DZ1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ1
+//
+//  Created by Vladimir Shchedrin on 23/12/2018.
+//  Copyright © 2018 Mary Scherbakova. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
